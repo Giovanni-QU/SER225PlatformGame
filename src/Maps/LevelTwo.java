4b94a67914da0cb24feddfa3ece55cd9d7743287
Tom <tgadacy@gmail.com>
2020-10-19 11:21:49 -0400

    first comiit

--- src/Maps/LevelTwo.java
+++ src/Maps/LevelTwo.java
@@ -2,11 +2,13 @@ package Maps;
 
 import Level.Map;
 import Tilesets.CommonTileset;
+import Tilesets.TestTileset;
 import Utils.Point;
 
 public class LevelTwo extends Map {
 
     public LevelTwo() {
-        super("level_one.txt", new CommonTileset(), new Point(1, 11));
+        super("level_one.txt", new TestTileset(), new Point(1, 11));
     }
+
 }

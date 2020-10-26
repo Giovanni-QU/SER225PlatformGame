1b4186862d3b973748640c3c21d86ba3394e65b8
AidanSchmid <Aidan.schmid@quinnipiac.edu>
2020-10-26 11:32:08 -0400

    Created Attack

--- src/Level/Enemy.java
+++ src/Level/Enemy.java
@@ -1,58 +1,62 @@
 package Level;
 
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
 import GameObject.Frame;
 import GameObject.ImageEffect;
 import GameObject.Rectangle;
 import GameObject.SpriteSheet;
 
-import java.awt.image.BufferedImage;
-import java.util.HashMap;
-
 // This class is a base class for all enemies in the game -- all enemies should extend from it
 public class Enemy extends MapEntity {
+	protected static MapEntityStatus removed;
 
-    public Enemy(float x, float y, SpriteSheet spriteSheet, String startingAnimation) {
-        super(x, y, spriteSheet, startingAnimation);
-    }
+	public Enemy(float x, float y, SpriteSheet spriteSheet, String startingAnimation) {
+		super(x, y, spriteSheet, startingAnimation);
 
-    public Enemy(float x, float y, HashMap<String, Frame[]> animations, String startingAnimation) {
-        super(x, y, animations, startingAnimation);
-    }
+	}
 
-    public Enemy(BufferedImage image, float x, float y, String startingAnimation) {
-        super(image, x, y, startingAnimation);
-    }
+	public Enemy(float x, float y, HashMap<String, Frame[]> animations, String startingAnimation) {
+		super(x, y, animations, startingAnimation);
+	}
 
-    public Enemy(BufferedImage image, float x, float y) {
-        super(image, x, y);
-    }
+	public Enemy(BufferedImage image, float x, float y, String startingAnimation) {
+		super(image, x, y, startingAnimation);
+	}
 
-    public Enemy(BufferedImage image, float x, float y, float scale) {
-        super(image, x, y, scale);
-    }
+	public Enemy(BufferedImage image, float x, float y) {
+		super(image, x, y);
+	}
 
-    public Enemy(BufferedImage image, float x, float y, float scale, ImageEffect imageEffect) {
-        super(image, x, y, scale, imageEffect);
-    }
+	public Enemy(BufferedImage image, float x, float y, float scale) {
+		super(image, x, y, scale);
+	}
 
-    public Enemy(BufferedImage image, float x, float y, float scale, ImageEffect imageEffect, Rectangle bounds) {
-        super(image, x, y, scale, imageEffect, bounds);
-    }
+	public Enemy(BufferedImage image, float x, float y, float scale, ImageEffect imageEffect) {
+		super(image, x, y, scale, imageEffect);
+	}
 
-    @Override
-    public void initialize() {
-        super.initialize();
-    }
+	public Enemy(BufferedImage image, float x, float y, float scale, ImageEffect imageEffect, Rectangle bounds) {
+		super(image, x, y, scale, imageEffect, bounds);
+	}
 
-    public void update(Player player) {
-        super.update();
-        if (intersects(player)) {
-            touchedPlayer(player);
-        }
-    }
+	@Override
+	public void initialize() {
+		super.initialize();
+	}
 
-    // A subclass can override this method to specify what it does when it touches the player
-    public void touchedPlayer(Player player) {
-        player.hurtPlayer(this);
-    }
+	public void update(Player player) {
+		super.update();
+		if (intersects(player)) {
+			touchedPlayer(player);
+		}
+	}
+
+	// A subclass can override this method to specify what it does when it touches
+	// the player
+	public void touchedPlayer(Player player) {
+
+		player.hurtPlayer(this);
+	}
 }
